86ff1f77b680d5ce70229539d228e48346c904f8
Caroline Kosalka <caroline.kosalka@gmail.com>
2022-07-14 18:12:51 +0200

    fixed the dag

--- dags/dummy_dag.py
+++ dags/dummy_dag.py
@@ -1,8 +1,7 @@
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.postgres_operator import PostgresOperator
 from datetime import datetime
 with DAG('example_dag', start_date=datetime(2022, 1, 1)) as dag:
     populate_db = PostgresOperator
-
-
     op = DummyOperator(task_id='op')
